f7660d20ec0f5d38d316e2c3bff3152fcf5ae86c
carolinamabad <158006775+carolinamabad@users.noreply.github.com>
2024-04-10 15:23:28 -0400

    "hello"

---
+++ mainpy.py
@@ -0,0 +1,6 @@
+def password_encoder(password):
+    encoded_password = ""
+    for digit in password:
+        encoded_digit = (int(digit) + 3) % 10
+        encoded_password += str(encoded_digit)
+    return encoded_password
